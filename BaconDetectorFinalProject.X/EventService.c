dbec4af644f20c9b05924f845c658f6bd9fe6490
Alexandria Liu <aliu81@ucsc.edu>
2022-05-17 23:33:52 -0700

    working on tl_tr_event equation but not completed yet

--- BaconDetectorFinalProject.X/EventService.c
+++ BaconDetectorFinalProject.X/EventService.c
@@ -435,6 +435,32 @@ uint8_t TCTEvent (void){
 }
 
 
+uint8_t TL_and_TR_Event(void){
+    static uint8_t CurrTapeTL;
+    CurrTape = BosshogReadTopCenterTape();
+    uint8_t WasEvent = FALSE;
+//    if(CurrTape != LastTCT && CurrTape == TAPE_BLACK){
+//        ES_Event TapeEvent;
+//        TapeEvent.EventType = TC_TAPE_BLACK;
+//        TapeEvent.EventParam = (uint16_t) CurrTape;
+//#ifndef EVENTCHECKER_TEST
+//        PostBosshogHSM(TapeEvent);
+//#else
+//        SaveEvent(BumperEvent);
+//#endif  
+//        WasEvent = TRUE;
+//    }else if(CurrTape != LastTCT && CurrTape == TAPE_WHITE){
+//        ES_Event TapeEvent;
+//        TapeEvent.EventType = TC_TAPE_WHITE;
+//        TapeEvent.EventParam = (uint16_t) CurrTape;
+//        PostBosshogHSM(TapeEvent); 
+//        WasEvent = TRUE;
+//    }
+//    LastTCT = CurrTape;
+    return WasEvent;
+}
+
+
 /**
  * @Function RunEventService(ES_Event ThisEvent)
  * @param ThisEvent - the event (type and param) to be responded.
@@ -479,6 +505,7 @@ ES_Event RunEventService(ES_Event ThisEvent)
         TLTEvent();
         TRTEvent();
         TCTEvent();
+        TL_and_TR_Event();
         //reset ES TIMER
         ES_Timer_InitTimer(EVENT_TIMER,5);
 //        if (batVoltage > BATTERY_DISCONNECT_THRESHOLD) { // is battery connected?
